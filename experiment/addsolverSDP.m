2821d80d8128c5eedc85b19dfb8daa34f1a916d7
TheoJJF <131817438+TheoJJF@users.noreply.github.com>
2025-03-08 02:06:28 -0800

    Update addsolverSDP.m

--- experiment/addsolverSDP.m
+++ experiment/addsolverSDP.m
@@ -1,11 +1,11 @@
 function [path,SDP1,SDP2] = addsolverSDP()
     clc;clear path;
-    path.sedumipath      = "C:\Users\itsth\solvers\sedumi";
-    path.manoptpath      = "C:\Users\itsth\solvers\manopt";
-    path.sdpt3path       = "C:\Users\itsth\solvers\SDPT3-4.0";
-    path.sdpnalpath      = "C:\Users\itsth\solvers\SDPNAL+v1.0";
-    path.mosekpath       = "C:\Program Files\Mosek";
-    path.stridepath      = "C:\Users\itsth\Desktop\STRIDE";
+    path.sedumipath      = "path/to/sedumi";
+    path.manoptpath      = "path/to/manopt";
+    path.sdpt3path       = "path/to/sdpt3";
+    path.sdpnalpath      = "path/to/sdpnal";
+    path.mosekpath       = "path/to/mosek";
+    path.stridepath      = "path/to/stride";
     
     SDP1 = load('../data/quasar_50_1.mat');    
     SDP2 = load('../data/quasar_100_1.mat');   
